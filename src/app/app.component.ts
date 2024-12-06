a32e8b42be014da14396aa2a3c49ef8e620680a4
paulameg <paulatakahashi4@gmail.com>
2024-12-05 21:38:26 -0300

    Main interface component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { WeatherService } from './services/weather.service';
+import { GeocodingService } from './services/geocoding.service';
+
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
+import { MapComponent } from './components/map/map.component';
+import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    SearchBarComponent,
+    WeatherCardComponent,
+    MapComponent,
+    CommonModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'bem-agro_weather-app';
+  weatherData: any;
+  latitude: any;
+  longitude: any;
+  hasSearched = false;
+  isLoading = false;
+
+
+  constructor(
+    private weatherService: WeatherService,
+    private geocodingService: GeocodingService
+  ) {}
+
+  fetchWeather(city: string): void {
+    this.isLoading = true;
+    this.weatherService.getWeather(city).subscribe(data => {
+      this.weatherData = data;
+      this.latitude = data.coord.lat;
+      this.longitude = data.coord.lon;
+      this.hasSearched = true;
+      this.isLoading = false;
+    },
+    (error) => {
+      console.error('Erro ao buscar dados do clima:', error);
+      this.isLoading = false;
+    });
+  }
 }
