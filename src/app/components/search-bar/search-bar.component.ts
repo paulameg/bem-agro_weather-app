41daa28c69fc749ec2c0e1baaeb07993bdff440c
paulameg <paulatakahashi4@gmail.com>
2024-12-05 21:18:45 -0300

    feat: Search bar field

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatFormFieldModule, MatInputModule,MatButtonModule,
+    MatIconModule, FormsModule],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.scss'
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent {
+  @Output() search = new EventEmitter<string>();
+  city: string = '';
+  weatherData: any = null;
 
+  onSearch() {
+    if (this.city.trim()) {
+      this.search.emit(this.city.trim());
+    }
+  }
 }
+
